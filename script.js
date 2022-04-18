2b849593f994007ef1448894cc1fc46df582045c
Eran Shalev <eranms2800@gmail.com>
2022-04-18 21:43:38 +0300

    Added Basic Movement rules

--- script.js
+++ script.js
@@ -1,119 +1,425 @@
+const DIMENSIONS = 8;
+const BLACK_PLAYER = "black";
+const WHITE_PLAYER = "white";
+let chess_board;
+
+class Piece {
+  constructor(row, col, color, type) {
+    this.row = row;
+    this.col = col;
+    this.color = color;
+    this.type = type;
+  }
+}
+
+
 window.onload = () => {
-    const dimensions = 8;
-    let main_container = document.createElement("div");
+  let main_container = document.createElement("div");
+  main_container.className = "table-container";
 
-    main_container.className = "table-container";
+  document.body.appendChild(main_container);
+  let play_board = document.createElement("table");
 
-    document.body.appendChild(main_container);
-    let play_board = document.createElement("table");
+  main_container.appendChild(play_board);
+  initializeBoard();
 
-    main_container.appendChild(play_board);
+  generateHeaders(play_board);
 
-    generateHeaders(play_board, dimensions + 1);
+  for (let i = DIMENSIONS - 1; i >= 0; i--) {
+    let row = document.createElement("tr");
+    play_board.appendChild(row);
+    chessRow(row, i);
+  }
 
-    for (let i = dimensions - 1; i >= 0; i--) {
-        let row = document.createElement("tr");
-        play_board.appendChild(row);
-        chessRow(row, i, dimensions);
-    }
+  generateHeaders(play_board);
 
-    generateHeaders(play_board, dimensions + 1);
 };
 
-function generateHeaders(board, max_range) {
-    for (let i = 0; i < max_range; i++) {
-        let header = document.createElement("th");
+function initializeBoard() {
+  const values = ["rook", "knight", "bishop", "queen", "king", "bishop","knight", "rook"];
 
-        header.textContent = String.fromCharCode("a".charCodeAt(0) + i - 1);
-        if (i === 0) {
-            header.textContent = "";
-        }
-        board.appendChild(header);
-    }
+  chess_board = Array(DIMENSIONS * DIMENSIONS).fill(null);
 
+  for (let colIndex = 0; colIndex < DIMENSIONS; colIndex++)
+   {
+    chess_board[colIndex + DIMENSIONS] = new Piece(1, colIndex, WHITE_PLAYER, "pawn");
 
+    chess_board[colIndex + (DIMENSIONS - 2) * DIMENSIONS] = new Piece(DIMENSIONS - 2, colIndex, BLACK_PLAYER, "pawn");
+
+    chess_board[colIndex] = new Piece(0, colIndex, WHITE_PLAYER, values[colIndex]);
+
+    chess_board[colIndex + DIMENSIONS * (DIMENSIONS - 1)] = new Piece(DIMENSIONS - 1, colIndex, BLACK_PLAYER, values[colIndex]);
+  }
 }
 
-function piecePlacer(rowIndex, columnIndex) {
-    if (rowIndex === 7) {
-        if (columnIndex === 0 || columnIndex === 7) {
-            return "url('black_rook.svg.png')";
-        } else if (columnIndex === 1 || columnIndex === 6) {
-            return "url('black_knight.svg.png')";
-        } else if (columnIndex === 2 || columnIndex === 5) {
-            return "url('black_bishop.svg.png')";
-        } else if (columnIndex === 3) {
-            return "url('black_queen.svg.png')";
-        } else {
-            return "url('black_king.svg.png')";
-        }
-    } else if (rowIndex === 6) {
-        return "url('black_pawn.svg.png')";
-    } else if (rowIndex === 1) {
-        return "url('white_pawn.svg.png')";
-    } else if (rowIndex === 0) {
-        if (columnIndex === 0 || columnIndex === 7) {
-            return "url('white_rook.svg.png')";
-        } else if (columnIndex === 1 || columnIndex === 6) {
-            return "url('white_knight.svg.png')";
-        } else if (columnIndex === 2 || columnIndex === 5) {
-            return "url('white_bishop.svg.png')";
-        } else if (columnIndex === 3) {
-            return "url('white_queen.svg.png')";
-        } else {
-            return "url('white_king.svg.png')";
-        }
-    }
+function generateHeaders(board) {
+  for (let i = 0; i < DIMENSIONS; i++) {
+    let header = document.createElement("th");
 
-    return "no_image";
+    header.textContent = String.fromCharCode("a".charCodeAt(0) + i - 1);
+    if (i === 0) {
+      header.textContent = "";
+    }
+    board.appendChild(header);
+  }
 }
 
-function chessRow(row, index, dimensions) {
-    let row_header = document.createElement("th");
-    row_header.textContent = index;
-    row.appendChild(row_header);
+function getImageSrc(type, color)
+{
+  return "assets/" + color + "_" + type + ".png";
+}
 
-    for (let i = 0; i < dimensions; i++) {
-        let tile = document.createElement("td");
-        let image = piecePlacer(index, i);
+function chessRow(row, index) {
+  let row_header = document.createElement("th");
+  row_header.textContent = index;
+  row.appendChild(row_header);
 
-        if (image !== "no_image") {
-            tile.style.backgroundImage = image;
-            tile.style.backgroundRepeat = "no-repeat";
-            tile.style.backgroundSize = "100% 100%";
-        }
-
-        if (!(index % 2)) {
-            tile.className = i % 2 ? "black-tile" : "white-tile";
-        } else {
-            tile.className = i % 2 ? "white-tile" : "black-tile";
-        }
-
-
-
-        tile.setAttribute('id', String(index + "-" + i));
-
-        tile.addEventListener('click', () => { tileClicked(index, i) });
-
-        row.appendChild(tile);
+  for (let i = 0; i < DIMENSIONS; i++) {
+    let tile = document.createElement("td");
+    let piece =chess_board[index * DIMENSIONS + i];
+    
+    if (piece)
+    {
+      let image = document.createElement("img");
+      image.src = getImageSrc(piece.type, piece.color);
+      tile.appendChild(image);      
     }
 
-    let end_header = document.createElement("th");
-    end_header.textContent = index;
-    row.appendChild(end_header);
+    if (!(index % 2)) {
+      tile.classList.add(i % 2 ? "black-tile" : "white-tile");
+    } else {
+      tile.classList.add(i % 2 ? "white-tile" : "black-tile");
+    }
+
+    tile.setAttribute("id", String(index + "-" + i));
+
+    tile.addEventListener("click", () => {
+      tileClicked(index, i);
+    });
+
+    row.appendChild(tile);
+  }
+
+  let end_header = document.createElement("th");
+  end_header.textContent = index;
+  row.appendChild(end_header);
+}
+
+function pawnMoves(pawn)
+{
+  if (pawn.color === WHITE_PLAYER)
+  {
+    if (pawn.row < DIMENSIONS - 1 && chess_board[(pawn.row + 1) * DIMENSIONS + pawn.col] === null)
+    {
+      return String((pawn.row + 1) + "-" + pawn.col + ",");
+    }
+  }
+  else
+  {
+    if (pawn.row > 0 && chess_board[(pawn.row - 1) * DIMENSIONS + pawn.col] === null)
+    {
+      return String ((pawn.row - 1) + "-" + pawn.col + ",");
+    }
+  }
+
+  return "-1";
+}
+
+function rookMoves(rook)
+{
+  let cords = "";
+  let col = rook.col;
+  let row = rook.row;
+
+  while (col > 0)
+  {
+    // if (chessRow[rook.row * DIMENSIONS + col - 1] === null)
+    // {
+      cords += String((rook.row) + "-"  +(col - 1) + ",");
+    // }
+    // else
+    // {
+    //   break;
+    // }
+
+    col--;
+  }
+
+  col = rook.col;
+  while (col < DIMENSIONS - 1)
+  {
+    // if (chessRow[rook.row * DIMENSIONS + col + 1] === null)
+    // {
+      cords += String((rook.row) + "-" + (col + 1) + ",");
+    // }
+    // else
+    // {
+    //   break;
+    // }
+
+    col++;
+  }
+
+  while (row < DIMENSIONS - 1)
+  {
+    cords += String((row + 1) + "-" + (rook.col) + ",");
+    row++;
+  }
+
+  row = rook.row;
+  while (row > 0)
+  {
+    cords += String((row - 1) + "-" + (rook.col) + ",");
+    row--;
+  }
+
+  if (cords === "")
+  {
+    cords = "-1";
+  }
+  return cords;
+}
+
+function bishopMoves(bishop)
+{
+  let cords = "";
+  let col = bishop.col;
+  let row = bishop.row;
+
+  while (col > 0 && row > 0)
+  {
+    col--;
+    row--;
+    cords += String(row + "-" + col + ",");
+  }
+
+  col = bishop.col;
+  row = bishop.row;
+
+  while (col < DIMENSIONS - 1 && row < DIMENSIONS - 1)
+  {
+    col++;
+    row++;
+    cords += String(row + "-" + col + ",");
+  }
+
+  col = bishop.col;
+  row = bishop.row;
+
+  while (col < DIMENSIONS - 1 && row > 0)
+  {
+    col++;
+    row--;
+    cords += String(row + "-" + col + ",");
+  }
+
+  col = bishop.col;
+  row = bishop.row;
+
+  while (col > 0 && row < DIMENSIONS - 1)
+  {
+    col--;
+    row++;
+    cords += String(row + "-" + col + ",");
+  }
+
+  if (cords === "")
+  {
+    cords = "-1";
+  }
+
+  return cords;
+}
+
+// function generateKnightCords(num1, num2, isOneCol)
+// {
+//   if (num1 + 2 < DIMENSIONS)
+//   {
+
+//   }
+// }
+
+function knightMoveHorizontal(row, col)
+{
+  
+}
+
+function knightMoves(knight)
+{
+  let cords = "";
+  let row = knight.row;
+  let col = knight.col;
+
+  if (col + 2 < DIMENSIONS)
+  {
+    if (row + 1 < DIMENSIONS)
+    {
+      cords += String((row + 1) + "-" + (col + 2) + ",");
+    }
+
+    if (row - 1 >= 0)
+    {
+      cords += String((row - 1) + "-" + (col + 2) + ",");
+    }
+  }
+
+  if (row + 2 < DIMENSIONS)
+  {
+    if (col + 1 < DIMENSIONS)
+    {
+      cords += String((row + 2) + "-" + (col + 1) + ",");
+    }
+
+    if (col - 1 >= 0)
+    {
+      cords += String((row + 2) + "-" + (col - 1) + ",");
+    }
+  }
+
+  if (col - 2 >= 0)
+  {
+    if (row + 1 < DIMENSIONS)
+    {
+      cords += String((row + 1) + "-" + (col - 2) + ",");
+    }
+
+    if (row - 1 >= 0)
+    {
+      cords += String((row - 1) + "-" + (col - 2) + ",");
+    }
+  }
+
+  if (row - 2 >= 0)
+  {
+    if (col + 1 < DIMENSIONS)
+    {
+      cords += String((row - 2) + "-" + (col + 1) + ",");
+    }
+
+    if (col - 1 >= 0)
+    {
+      cords += String((row - 2) + "-" + (col - 1) + ",");
+    }
+  }
+
+
+  if (cords === "")
+  {
+    cords = "-1";
+  }
+
+  return cords;
+}
+
+function kingRow(row, col)
+{
+  let cords = "";
+  if (col - 1 >= 0)
+  {
+    cords += String((row) + "-" + (col - 1) + ",");
+  }
+  if (col + 1 < DIMENSIONS)
+  {
+    cords += String((row) + "-" + (col + 1) + ",");
+
+  }
+
+  return cords;
+}
+
+function kingMoves(king)
+{
+  let cords = "";
+  let row = king.row;
+  let col = king.col;
+
+  cords += kingRow(row, col);
+
+  if (row + 1 < DIMENSIONS)
+  {
+    cords += kingRow(row + 1, col)
+    cords += String((row + 1) + "-" + (col) + ",");
+  }
+
+  if (row - 1 >= 0)
+  {
+    cords += kingRow(row - 1, col);
+    cords += String((row - 1) + "-" + (col) + ",");
+  }
+
+  if (cords === "")
+  {
+    cords = "-1";
+  }
+
+  return cords;
+}
+
+function availableMoves(chessPiece)
+{
+  if (chessPiece.type === "pawn")
+  {
+    return pawnMoves(chessPiece);
+  }
+  else if (chessPiece.type === "rook")
+  {
+    return rookMoves(chessPiece);
+  }
+  else if (chessPiece.type === "bishop")
+  {
+    return bishopMoves(chessPiece);
+  }
+  else if (chessPiece.type === "queen")
+  {
+    return bishopMoves(chessPiece) + rookMoves(chessPiece);
+  }
+  else if (chessPiece.type === "knight")
+  {
+    return knightMoves(chessPiece);
+  }
+  else {
+    return kingMoves(chessPiece);
+  }
+}
+
+function resetMarkers()
+{
+  for (let tile of document.getElementsByTagName("td"))
+  {
+    tile.classList.remove("optional-move");
+  }
 }
 
 function tileClicked(rowIndex, columnIndex) {
-    for (let i = 0; i < 8; i++) {
-        for (let j = 0; j < 8; j++) {
-            tile = document.getElementById(String(i + "-" + j));
-            color = getComputedStyle(tile).borderColor;
+  let selectedTile = document.getElementById(
+    String(rowIndex + "-" + columnIndex)
+  );
 
-            if (color !== "rgb(0, 0, 0)") {
-                tile.style.borderColor = "rgb(0, 0, 0)";
-            }
-        }
+  resetMarkers();
+
+  let previousTile = document.getElementsByClassName("selected")[0];
+
+  if (previousTile) {
+    previousTile.classList.remove("selected");
+  }
+
+  selectedTile.classList.add("selected");
+
+  let piece = chess_board[rowIndex * DIMENSIONS + columnIndex];
+  
+  if (piece)
+  {
+    let tileCords = availableMoves(piece);
+
+    if (tileCords !== "-1")
+    {
+      tileCords = tileCords.split(",");
+      tileCords.pop();
+      
+      for (let cord of tileCords)
+      {
+        document.getElementById(cord).classList.add("optional-move");
+      }
     }
-
-    document.getElementById(String(rowIndex + "-" + columnIndex)).style.borderColor = "rgb(255, 0, 0)";
+  }
 }
